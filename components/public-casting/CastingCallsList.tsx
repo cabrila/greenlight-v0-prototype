19c0a85612895faaa94f385cf5900292314a02bc
v0agent <it+v0agent@vercel.com>
2026-06-02 13:34:19 +0000

    feat: rename button title to "Edit Casting Call" in CastingCallsList component

--- components/public-casting/CastingCallsList.tsx
+++ components/public-casting/CastingCallsList.tsx
@@ -286,7 +286,7 @@ export default function CastingCallsList({
               <button
                 onClick={(e) => handleEditProject(e, project)}
                 className="p-1.5 bg-white/10 hover:bg-white/20 rounded-lg text-white/70 hover:text-white transition-colors"
-                title="Edit Project"
+                title="Edit Casting Call"
               >
                 <FolderEdit className="w-4 h-4" />
               </button>
